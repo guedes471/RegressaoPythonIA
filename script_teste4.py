79886c3a59b5a53af961aee820b6604469ef70ae
guedes471 <55673363+guedes471@users.noreply.github.com>
2022-03-18 23:45:06 -0300

    Add files via upload

---
+++ script_teste4.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPRegressor
+
+print('Carregando Arquivo de teste')
+arquivo = np.load('teste4.npy')
+x = arquivo[0]
+y = np.ravel(arquivo[1])
+
+
+
+
+
+regr = MLPRegressor(hidden_layer_sizes=(20),
+                    max_iter=150000,
+                    activation='relu', #{'identity', 'logistic', 'tanh', 'relu'},
+                    solver='adam',
+                    learning_rate = 'adaptive',
+                    n_iter_no_change=100)
+print('Treinando RNA')
+regr = regr.fit(x,y)
+
+
+
+print('Preditor')
+y_est = regr.predict(x)
+
+erros = []
+for i in range(10):
+    regr2 = regr.fit(x,y)
+    y_est2 = regr.predict(x)
+    erros.append(regr2.loss_)
+media = sum(erros) / 10
+print("Media:", media)
+print("Desvio padrao:", str(np.std(erros)))
+
+
+
+
+plt.figure(figsize=[14,7])
+
+#plot curso original
+plt.subplot(1,3,1)
+plt.plot(x,y)
+
+#plot aprendizagem
+plt.subplot(1,3,2)
+plt.plot(regr.loss_curve_)
+
+#plot regressor
+plt.subplot(1,3,3)
+plt.plot(x,y,linewidth=1,color='yellow')
+plt.plot(x,y_est,linewidth=2)
+
+
+
+
+plt.show()
